3d7bf5f8f3c6ddf6f19913f334a99e61d4aa4f96
Marc Mills <mdmills1990@gmail.com>
2018-09-11 16:59:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Owner
-  # code goes here
+@@all = []
+attr_accessor :pets, :name
+attr_reader :species
+
+def initialize(pets)
+  @pets = {fishes: [], cats: [], dogs: []}
+  @species = "human"
+end
+
+def self.all
+  @@all << self.new
+end
+
+def self.count
+end
+
+def self.reset_all
+end
+
+def say_species
+"I am a #{@species}."
+end
+
+def buy_cat
+end
+
+def buy_dog
+end
+
+def buy_fish
+end
+
+def walk_dogs
+end
+
+def feed_fish
+end
+
+def sell_pets
+end
+
+def list_pets
+end
+
 end
