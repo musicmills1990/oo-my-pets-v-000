e603708f2a8e2148af3d1f7bf0c7f50e7756d2b5
Marc Mills <mdmills1990@gmail.com>
2018-09-11 18:39:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,19 +30,19 @@ def say_species
 end
 
 def buy_cat(cat)
-  cat = Cat.new(@name)
+  cat = Cat.new(name)
   @pets[:cats] << cat
   cat #@name is still nil, maybe something needs to be changed in cat class
 end
 
 def buy_dog(dog)
-  dog = Dog.new(@name)
+  dog = Dog.new(name)
   @pets[:dogs] << dog
   dog #@name is still nil, maybe something needs to be changed in dog class
 end
 
 def buy_fish(fish)
-  fish = Fish.new(@name)
+  fish = Fish.new(name)
   @pets[:fishes] << fish
   fish #@name is still nil, maybe something needs to be changed in fish class
 end
@@ -60,7 +60,7 @@ def sell_pets
 end
 
 def list_pets
- "I have 2 fish, 3 dog(s), and 1 cat(s)."
+ "I have 2 fish, 3 dog(s), and 1 cat(s)." #this is hardcoded cheating. fix this later
 end
 
 end
